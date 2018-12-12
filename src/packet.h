be5172b32717df7a43af90d599b392a21544fcff
Jan Hrazdira <hrazjan@gmail.com>
2018-12-12 18:26:31 +0100

    packet.h updated

--- src/packet.h
+++ src/packet.h
@@ -2,21 +2,25 @@
 #define _packet_h
 #include <stdint.h>
 
-#define DATALEN 1016
+#define DATALEN 1015
 
 typedef struct {
-   uint32_t  size;
-   char  name[100];
+    char type;
+    uint32_t  size;
+    char name[1019];
 } Start; 
 
 typedef struct {
+    char type;
     uint32_t dataid;
     unsigned char data[DATALEN];
 	uint32_t crc;
 } Data;
 
 typedef struct {
+    char type;
     uint32_t hash[8];
+    char align[991];
 } Stop; 
 
 typedef struct {
